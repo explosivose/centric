01ab9825aa854489ce989b38161e22f7dc31806c
mattblickem <explosivose@gmail.com>
2014-02-18 00:13:29 +0000

    initial prototype

---
+++ Assets/CircleMaker.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CircleMaker : MonoBehaviour 
+{
+
+	public float radius = 1f;
+	public int vertexCount = 16;
+	public float lineStartWidth = 0.1f;
+	public float lineEndWidth = 0.5f;
+	public Material material;
+	
+	public int v;
+	
+	private LineRenderer line;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		line = gameObject.AddComponent<LineRenderer>();
+		line.material = material;
+		line.SetWidth(lineStartWidth, lineEndWidth);
+		
+		line.SetVertexCount(vertexCount+1);
+		
+		for (int i = 0; i < vertexCount + 1; i++)
+		{
+			float theta = (i * Mathf.PI * 2f) / vertexCount;
+			float x = radius * Mathf.Cos(theta);
+			float y = radius * Mathf.Sin(theta);
+			Vector3 pos = new Vector3(x, y);
+			line.SetPosition(i,pos);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+	public float orbitSpeed = 10f;
+	public int maxRadius = 5;
+	public int minRadius = 2;
+	
+	private float theta = 0f;
+	private float radius;
+	private int circle;
+	private enum LaneChange {up, down};
+	private bool changingLane = false;
+	// Use this for initialization
+	void Start () 
+	{
+		radius = minRadius;
+		circle = minRadius;
+	}
+	float yIn;
+	// Update is called once per frame
+	void Update () 
+	{
+		
+		 yIn = Input.GetAxisRaw("Vertical");
+		if (! changingLane)
+		{
+			if (yIn > 0f && circle < maxRadius) StartCoroutine( ChangeRadius( (float)(circle + 1) ) );
+			if (yIn < 0f && circle > minRadius) StartCoroutine( ChangeRadius( (float)(circle - 1) ) );
+		}
+
+		float xIn = Input.GetAxis("Horizontal");
+		theta -= xIn * orbitSpeed * Time.deltaTime;
+		
+
+		float x = radius * Mathf.Cos(theta);
+		float y = radius * Mathf.Sin(theta);
+		Vector3 pos = new Vector3(x, y);
+		transform.position = pos;
+	}
+	
+	IEnumerator ChangeRadius(float newR)
+	{
+		
+		changingLane = true;
+		
+		float oldR = radius;
+		float s = Time.time;
+		while (!Mathf.Approximately(radius, newR))
+		{
+			radius = Mathf.Lerp (oldR, newR, (Time.time - s)*orbitSpeed);
+			yield return new WaitForFixedUpdate();
+		}
+		radius = newR;
+		circle = (int) newR;
+		changingLane = false;
+	}
+	
+}
