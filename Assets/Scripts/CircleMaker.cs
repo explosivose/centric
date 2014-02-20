389bed6937183d8f49e8b8115d1e47289b1d78a7
mattblickem <explosivose@gmail.com>
2014-02-20 09:43:51 +0000

    Added comments for CircleMaker

--- Assets/Scripts/CircleMaker.cs
+++ Assets/Scripts/CircleMaker.cs
@@ -1,28 +1,35 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/*  CircleMaker MonoBehaviour
+ *  Creates a line renderer in a circle
+ *  
+ */
+
 public class CircleMaker : MonoBehaviour 
 {
 
-	public float radius = 1f;
+	public float radius = 1f;			
 	public int vertexCount = 16;
 	public float lineStartWidth = 0.1f;
 	public float lineEndWidth = 0.5f;
 	public Material material;
 	
-	public int v;
-	
 	private LineRenderer line;
 	
-	// Use this for initialization
+	// Line is created in Start and never modified afterwards
+	// In future a we could have functions that modify the circle points etc.
 	void Start () 
 	{
+		// Initialize LineRenderer
 		line = gameObject.AddComponent<LineRenderer>();
 		line.material = material;
 		line.SetWidth(lineStartWidth, lineEndWidth);
-		
 		line.SetVertexCount(vertexCount+1);
 		
+		// Define LineRenderer points
+		// Here, a circle is defined on cartesian plane X/Y 
+		// ... using the parametric form "x = a + r cos(t), y = a + r sin(t)"
 		for (int i = 0; i < vertexCount + 1; i++)
 		{
 			float theta = (i * Mathf.PI * 2f) / vertexCount;
