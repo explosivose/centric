70e18d59f4e6acdfcbd890156fbeaf9e29771ef1
zerofiftyone <ben@zerofiftyone.net>
2014-03-04 02:13:33 +0000

    New movement implementation?
    Well it's an attempt anyway :P. Having some trouble with the movement
    script.
    
    Currently I'm able to make the player move at an even speed regardless
    of distance from centre, but SOMETHING is failing causing the player to
    movement to slowly spiral out of orbit. This has been giving me
    headaches.
    
    Also implemented is lane movement. It's currently a little sloppy, as
    the values aren't exact at the moment. However, the movement itself has
    a little bounce to it like in the mockup video, which is nice. The
    script also tracks which lane the player in so that they aren't able to
    move out of the rings.
    
    Note: I'm sure some of this may read quite badly or fail to make sense.
    I just realised 2am has passed and I'm incredibly drowsy. Night night i
    guess.

---
+++ Assets/Scripts/circularpath.js
@@ -0,0 +1,19 @@
+﻿var speed : float = 6.0;
+var target : Transform;
+private var moveDirection : Vector3 = Vector3.zero;
+
+function Update()
+{
+        var controller : CharacterController = GetComponent(CharacterController);
+        moveDirection = Vector3(Input.GetAxis("Horizontal"), 0,0);
+        var worldLookDirection = target.position - transform.position;
+        var localLookDirection = transform.InverseTransformDirection(worldLookDirection);
+        localLookDirection.y = 0;
+        transform.forward = transform.rotation * localLookDirection;
+        moveDirection = transform.TransformDirection(moveDirection);
+       
+        moveDirection *= speed;
+    
+        // Move the controller
+        controller.Move(moveDirection * Time.deltaTime);
+}
